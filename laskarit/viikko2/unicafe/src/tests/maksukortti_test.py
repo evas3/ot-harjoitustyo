ce94184e53c27750468be21cf2dde177bb963950
evas <evamaria.asplund@gmail.com>
2023-03-22 17:07:39 +0200

    uudet

--- laskarit/viikko2/unicafe/src/tests/maksukortti_test.py
+++ laskarit/viikko2/unicafe/src/tests/maksukortti_test.py
@@ -7,3 +7,18 @@ class TestMaksukortti(unittest.TestCase):
 
     def test_luotu_kortti_on_olemassa(self):
         self.assertNotEqual(self.maksukortti, None)
+
+    def kortin_saldo_alussa(self):
+        self.assertEqual(str(self.maksukortti), "Kortilla on rahaa 10 euroa")
+
+    def rahan_lataaminen(self):
+        self.maksukortti.lataa_rahaa(self, 1000)
+        self.assertEqual(str(self.maksukortti), "Kortilla on rahaa 20 euroa")
+
+    def rahan_ottaminen_saldoa_tarpeeksi(self):
+        self.maksukortti.ota_rahaa(self, 1000)
+        self.assertEqual(self.maksukortti.ota_rahaa, True)
+
+    def rahan_ottaminen_saldoa_liianvahan(self):
+        self.maksukortti.ota_rahaa(self, 2000)
+        self.assertEqual(self.maksukortti.ota_rahaa, False)
