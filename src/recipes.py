c17a850e48e2e793b4827916363a4c46353fed25
evas3 <evamaria.asplund@gmail.com>
2023-04-25 23:02:30 +0300

    englanniksi

--- src/recipes.py
+++ src/recipes.py
@@ -2,54 +2,51 @@ import random
 
 class Recipes():
     def __init__(self):
-        self.meal = ["keittoresepti", "sopparesepti", "soossiresepti"]
-        self.dessert = ["kakkuresepti", "piirasresepti", "tortturesepti"]
-        self.tiedosto = '/home/evas/ot-harjoitustyo/src/ainesosasia.txt'
-        print("Itse et osaa tehdä hyviä päätöksiä -minä voin päättää reseptit puolestasi")
-        print("Tekeekö mieli kokata vai leipoa? Paina entteriä jos et osaa päättää")
+        self.meal = ["/home/evas/ot-harjoitustyo/src/nuudelit.txt", "/home/evas/ot-harjoitustyo/src/keitto.txt", "/home/evas/ot-harjoitustyo/src/hampurilaiset.txt"]
+        self.dessert = ["/home/evas/ot-harjoitustyo/src/mansikkakakku.txt", "/home/evas/ot-harjoitustyo/src/mustikkapiirakka.txt", "/home/evas/ot-harjoitustyo/src/kaaretorttu.txt"]
+        print("Can't make good decisions? I can!")
+        print("Would you like to bake or cook? Press enter if you don't know.")
 
     def welcome(self, sweet_or_salty):
-        if sweet_or_salty.lower() == "m":
-            return reseptit.sweet()
-        elif sweet_or_salty.lower() == "s":
-           return reseptit.salty()
+        if sweet_or_salty.lower() == "b":
+            return recepies.sweet()
+        elif sweet_or_salty.lower() == "c":
+           return recepies.salty()
         elif sweet_or_salty == "":
-            choises = [reseptit.sweet, reseptit.salty]
+            choises = [recepies.sweet, recepies.salty]
             return random.choice(choises)()
         else:
             print()
-            print("Yritetäänpä uudelleen")
-            sweet_or_salty = str(input("Makeaa (paina m-kirjainta) vai Suolaista(paina s-kirjainta): "))
-            return reseptit.welcome(sweet_or_salty)
+            print("Try that again")
+            sweet_or_salty = str(input("Bake (press 'b') or cook (press 'c'): "))
+            return recepies.welcome(sweet_or_salty)
 
     def sweet(self):
-        print(reseptit.__str__(self.dessert))
-        with open(self.tiedosto) as tiedosto:
-            for rivi in tiedosto:
-                print(rivi)
-        reseptit.redo()
+        print()
+        with open(random.choice(self.dessert)) as file:
+            for row in file:
+                print(row)
+        recepies.redo()
 
     def salty(self):
-        print(reseptit.__str__(self.meal))
-        with open(self.tiedosto) as tiedosto:
-            for rivi in tiedosto:
-                print(rivi)
-        reseptit.redo()
+        print()
+        with open(random.choice(self.meal)) as file:
+            for row in file:
+                print(row)
+        recepies.redo()
 
     def redo(self):
         print()
-        print("Haetko kenties jotakin muuta?")
-        again = input("Valitaanko uusi resepti? Paina y-kirjainta niin yritetään uudelleen.")
+        print("Anything else?")
+        again = input("Would you like to choose another recipe? Press 'y' to try again.")
         if again.lower() == "y":
-            sweet_or_salty = str(input("Makeaa (paina m-kirjainta) vai Suolaista(paina s-kirjainta): "))
-            reseptit.welcome(sweet_or_salty)
+            sweet_or_salty = str(input("Bake (press 'b') or cook (press 'c'): "))
+            recepies.welcome(sweet_or_salty)
         else:
-            print("Ole hyvä ja näkemiin!")
+            print("OYou're welcome!")
         
-    def __str__(self, list):
-        return random.choice(list)
 
 if __name__ ==  "__main__":
-    reseptit = Recipes()
-    sweet_or_salty = str(input("Makeaa (paina m-kirjainta) vai Suolaista(paina s-kirjainta): "))
-    reseptit.welcome(sweet_or_salty)
+    recepies = Recipes()
+    sweet_or_salty = str(input("Bake (press 'b') or cook (press 'c'): "))
+    recepies.welcome(sweet_or_salty)
