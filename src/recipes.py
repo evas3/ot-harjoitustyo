df17d8a005a7f4f71bbd1184770b56a0bad75679
evas3 <evamaria.asplund@gmail.com>
2023-04-25 23:53:49 +0300

    virheenkorjausta

--- src/recipes.py
+++ src/recipes.py
@@ -1,9 +1,14 @@
 import random
 
+
 class Recipes():
     def __init__(self):
-        self.meal = ["/home/evas/ot-harjoitustyo/src/nuudelit.txt", "/home/evas/ot-harjoitustyo/src/keitto.txt", "/home/evas/ot-harjoitustyo/src/hampurilaiset.txt"]
-        self.dessert = ["/home/evas/ot-harjoitustyo/src/mansikkakakku.txt", "/home/evas/ot-harjoitustyo/src/mustikkapiirakka.txt", "/home/evas/ot-harjoitustyo/src/kaaretorttu.txt"]
+        self.meal = ["/home/evas/ot-harjoitustyo/src/nuudelit.txt",
+                     "/home/evas/ot-harjoitustyo/src/keitto.txt",
+                     "/home/evas/ot-harjoitustyo/src/hampurilaiset.txt"]
+        self.dessert = ["/home/evas/ot-harjoitustyo/src/mansikkakakku.txt",
+                        "/home/evas/ot-harjoitustyo/src/mustikkapiirakka.txt",
+                        "/home/evas/ot-harjoitustyo/src/kaaretorttu.txt"]
         print("Can't make good decisions? I can!")
         print("Would you like to bake or cook? Press enter if you don't know.")
 
@@ -11,26 +16,27 @@ class Recipes():
         if sweet_or_salty.lower() == "b":
             return recepies.sweet()
         elif sweet_or_salty.lower() == "c":
-           return recepies.salty()
+            return recepies.salty()
         elif sweet_or_salty == "":
             choises = [recepies.sweet, recepies.salty]
             return random.choice(choises)()
         else:
             print()
             print("Try that again")
-            sweet_or_salty = str(input("Bake (press 'b') or cook (press 'c'): "))
+            sweet_or_salty = str(
+                input("Bake (press 'b') or cook (press 'c'): "))
             return recepies.welcome(sweet_or_salty)
 
     def sweet(self):
         print()
-        with open(random.choice(self.dessert)) as file:
+        with open(random.choice(self.dessert), "r") as file:
             for row in file:
                 print(row)
         recepies.redo()
 
     def salty(self):
         print()
-        with open(random.choice(self.meal)) as file:
+        with open(random.choice(self.meal), "r") as file:
             for row in file:
                 print(row)
         recepies.redo()
@@ -38,15 +44,17 @@ class Recipes():
     def redo(self):
         print()
         print("Anything else?")
-        again = input("Would you like to choose another recipe? Press 'y' to try again.")
+        again = input(
+            "Would you like to choose another recipe? Press 'y' to try again.")
         if again.lower() == "y":
-            sweet_or_salty = str(input("Bake (press 'b') or cook (press 'c'): "))
+            sweet_or_salty = str(
+                input("Bake (press 'b') or cook (press 'c'): "))
             recepies.welcome(sweet_or_salty)
         else:
-            print("OYou're welcome!")
-        
+            print("You're welcome!")
 
-if __name__ ==  "__main__":
+
+if __name__ == "__main__":
     recepies = Recipes()
-    sweet_or_salty = str(input("Bake (press 'b') or cook (press 'c'): "))
-    recepies.welcome(sweet_or_salty)
+    SWEET_OR_SALTY = str(input("Bake (press 'b') or cook (press 'c'): "))
+    recepies.welcome(SWEET_OR_SALTY)
--- src/tests/resepti_test.py
+++ src/tests/resepti_test.py
@@ -6,8 +6,18 @@ class TestRecipes(unittest.TestCase):
     def setUp(self):
         self.recipe = Recipes()
 
-    def test_welcome_input_m(self):
-        self.assertEqual(self.recipe.welcome("m"), recipe.sweet())
+    def test_welcome_input_b(self):
+        self.assertEqual(self.recipe.welcome("b"), recipe.sweet())
+
+    def test_welcome_input_c(self):
+        self.assertEqual(self.recipe.welcome("c"), recipe.salty())
+
+    def test_welcome_input_(self):
+        self.assertEqual(self.recipe.welcome(""), recipe.sweet())
+
+    def test_welcome_input_x(self):
+        self.assertEqual(self.recipe.welcome("x"), recipe.sweet())
+
 
 if __name__ == "__main__":
     unittest.main()
